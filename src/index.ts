1a2caa3c1e87d1ecde46d9339923f3f68732f1f8
大雾 <dawufenfen@gmail.com>
2022-02-22 16:35:32 +0800

    step 1:init game

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,29 @@
-import 'phaser';
+import "phaser";
 
 class GameScene extends Phaser.Scene {
-    
-    preload() {
-        this.load.image('ball', 'assets/why.png')
-    }
-    create() {
 
-    }
 }
 
+/* 游戏设置 */
 const config: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    backgroundColor: '#eee',
-    pixelArt: true,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
+  type: Phaser.AUTO,
+  width: 1200,
+  height: 600,
+  backgroundColor: "#eee",
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
     },
-    scale: {
-        mode: Phaser.Scale.ScaleModes.FIT,
-        autoCenter: Phaser.Scale.Center.CENTER_BOTH
-    },
-    scene: GameScene
-}
+  },
+  scale: {
+    mode: Phaser.Scale.ScaleModes.FIT,
+    autoCenter: Phaser.Scale.Center.CENTER_BOTH,
+  },
+  scene: GameScene,
+};
 
 window.onload = () => {
-    const game = new Phaser.Game(config)
-}
-console.log("this is end")
+  new Phaser.Game(config);
+};
