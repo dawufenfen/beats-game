c65ac0e55530bd340e7cd68c9f72c728696de2e8
大雾 <dawufenfen@gmail.com>
2022-02-22 16:57:21 +0800

    step 3: add physics ground

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,21 @@ class GameScene extends Phaser.Scene {
   /* 加载资源 */
   preload() {
     this.load.image("journey", "assets/journey.jpg");
+    this.load.image("ground", "assets/ground.jpg");
   }
 
   create() {
     /* 背景 */
     this.add.image(0, 0, "journey").setDisplaySize(1200, 600).setOrigin(0, 0);
+
+    /* 物理初始化，并放上几块木板 */
+    platforms = this.physics.add.staticGroup();
+    platforms.create(100, 300, "ground").setDisplaySize(200, 20).refreshBody();
+    platforms.create(1100, 400, "ground").setDisplaySize(200, 20).refreshBody();
+    platforms.create(600, 500, "ground").setDisplaySize(200, 20).refreshBody();
+    platforms.create(300, 400, "ground").setDisplaySize(200, 20).refreshBody();
+    platforms.create(800, 200, "ground").setDisplaySize(200, 20).refreshBody();
+    platforms.create(600, 590, "ground").setDisplaySize(1200, 20).refreshBody();
   }
 
   update() {
@@ -39,3 +49,6 @@ const config: Phaser.Types.Core.GameConfig = {
 window.onload = () => {
   new Phaser.Game(config);
 };
+
+/* 碰撞的平台 */
+var platforms: Phaser.Physics.Arcade.StaticGroup;
